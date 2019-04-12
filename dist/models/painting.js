65cd6272287cdf4fe4a13679dc834edb89d742b6
juanf9224 <juanfh.24.6@gmail.com>
2019-04-11 23:10:49 -0400

    - create dockerfile - add new start commands

--- dist/models/painting.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const PaintingSchema = new mongoose_1.Schema({
-    name: String,
-    url: String,
-    technique: String
-});
-exports.default = mongoose_1.model('Painting', PaintingSchema);
--- src/connection/ConnectionUtil.ts
+++ src/connection/ConnectionUtil.ts
@@ -5,9 +5,9 @@ const pwd = 'admin';
 const db = 'test?retryWrites=true';
 const url = '@powerfull-api-mongodb-cluster-gbmsd.mongodb.net/';
 const connectionString = `mongodb+srv://${user}:${pwd}${url}${db}`;
-const connectionStringLocal = 'mongodb://localhost:27017/test';
+// const connectionStringLocal = 'mongodb://localhost:27017/test';
 const ConnectionUtil = {
-    mongooseConnect: () => mongoose.connect(connectionStringLocal, { useNewUrlParser: true })
+    mongooseConnect: () => mongoose.connect(connectionString, { useNewUrlParser: true })
                             .catch(err => console.error('could not connect to database: ', err)),
     mongooseConnection: () => mongoose.connection.once('open', () => {
         console.log('Connected to database');
